f1ef04ac4504331f2bf55dc436b0af2b7c86f2c8
r1cs <1027309561@qq.com>
2021-07-01 22:51:31 +0800

    core/vm/ovm_state_manager.go: add height for diffdb have the same height as the header

--- l2geth/core/vm/ovm_state_manager.go
+++ l2geth/core/vm/ovm_state_manager.go
@@ -142,7 +142,7 @@ func putContractStorage(evm *EVM, contract *Contract, args map[string]interface{
 		before := evm.StateDB.GetState(address, key)
 		evm.StateDB.SetState(address, key, val)
 		err := evm.StateDB.SetDiffKey(
-			evm.Context.BlockNumber,
+			evm.Height,
 			address,
 			key,
 			before != val,
@@ -166,7 +166,7 @@ func testAndSetAccount(evm *EVM, contract *Contract, args map[string]interface{}
 
 	if evm.Context.EthCallSender == nil {
 		err := evm.StateDB.SetDiffAccount(
-			evm.Context.BlockNumber,
+			evm.Height,
 			address,
 		)
 
@@ -199,7 +199,7 @@ func testAndSetContractStorage(evm *EVM, contract *Contract, args map[string]int
 
 	if evm.Context.EthCallSender == nil {
 		err := evm.StateDB.SetDiffKey(
-			evm.Context.BlockNumber,
+			evm.Height,
 			address,
 			key,
 			changed,
